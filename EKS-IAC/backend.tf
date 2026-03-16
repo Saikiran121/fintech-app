54702a800d515ffc9160c33c9546d0d275925420
Saikiran Biradar <saikiranbiradar76642@gmail.com>
2026-03-16 17:07:59 +0530

    ci: update S3 bucket name in backend.tf for Terraform state management

--- EKS-IAC/backend.tf
+++ EKS-IAC/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = "fintech-s3-latest"
+    bucket       = "saikiran-s3-demo-v1.0"
     key          = "eks/terraform.tfstate"
     region       = "ap-south-1"
     encrypt      = true
