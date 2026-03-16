8bf7496f0a141e62bef87102cc538145da59430d
Saikiran Biradar <saikiranbiradar76642@gmail.com>
2026-03-16 16:57:07 +0530

    ci: update Jenkinsfile to include AWS credentials for Terraform init step

--- EKS-IAC/backend.tf
+++ EKS-IAC/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "fintech-s3-latest"
-    key            = "eks/terraform.tfstate"
-    region         = "ap-south-1"
-    encrypt        = true
-    dynamodb_table = "terraform-state-lock"
+    bucket       = "fintech-s3-latest"
+    key          = "eks/terraform.tfstate"
+    region       = "ap-south-1"
+    encrypt      = true
+    use_lockfile = true
   }
 }
